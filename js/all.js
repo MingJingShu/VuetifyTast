7d0f6ccae11f400ada1a645b017dbf36da9f2875
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2020-01-21 17:49:20 +0800

    First gogo

---
+++ js/all.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    // console.log("hello This is {all.js}");
+  
+
+
+    const app3 = new Vue({
+      el: '#app',
+      vuetify: new Vuetify(),
+      data: () => ({
+        drawer: null
+
+      })//data
+    })//Vue
+
+
+});//total
+//el = element
+//template 樣板
+//data change content
