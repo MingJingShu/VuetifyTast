93ce960dc691828ced41ab49d717ab63a28dacad
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2020-01-22 11:58:20 +0800

    Text test

--- js/all.js
+++ js/all.js
@@ -17,3 +17,19 @@ $(document).ready(function(){
 //el = element
 //template 樣板
 //data change content
+
+// =>
+const array1 =[3,0,6,7,8];
+const map1 = array1.map(x => x * 2);
+console.log('map1 ='+map1);
+
+
+const materials = [
+  'Hydrogen',
+  'Helium',
+  'Lithium',
+  'Beryllium'
+];
+
+console.log(materials.map(material => material.length));
+// expected output: Array [8, 6, 7, 9]
